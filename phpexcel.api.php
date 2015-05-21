cf74ecd61274caf3e50f6fc498b3ebe4602f0d2d
Wouter Admiraal <wad@wadmiraal.net>
2015-05-21 14:38:26 +0200

    Update the module documentation. Add examples and explanations.

--- phpexcel.api.php
+++ phpexcel.api.php
@@ -14,6 +14,8 @@
  * behaves in a "Drupal" way. This module simplifies the export or import of
  * data, abstracting much of the complexity, at the cost of flexibility.
  *
+ * @section export Exporting data
+ *
  * The idea is to provide an API very similar to Drupal's theme_table()
  * function.
  *
@@ -38,11 +40,226 @@
  * ), 'path/to/file.xls', array('description' => "Some description"));
  * @endcode
  *
- * Look at phpexcel_export() for more information on the parameters and their
- * possible values.
+ * If the target file already exists, data will be appended to it, instead of
+ * overwriting its content. It is also possible to use an existing file as a
+ * template. This is done by specifying the "template" option:
+ * @code
+ * phpexcel_export(array('Header 1', 'Header 2'), array(
+ *   array('A1', 'B1'),
+ *   array('A2', 'B2'),
+ * ), 'path/to/file.xls', array('template' => 'path/to/template.xls'));
+ * @endcode
  *
- * @todo Document return values
- * @todo Document import
+ * It is possible to export data to multiple worksheets. In that case, the
+ * headers array becomes a 2-dimensional array, and the data takes 3
+ * dimensions. The 1st dimension represents the Worksheets. They can be
+ * keyed by name, or simply numerically. The headers array determines the
+ * worksheet names, unless the "ignore_headers", in which case worksheet names
+ * are determined by the data array.
+ * @code
+ * phpexcel_export(
+ *   array('Worksheet 1' => array(
+ *     'Header 1',
+ *     'Header 2',
+ *   )),
+ *   array(array(
+ *     array('A1', 'B1'),
+ *     array('A2', 'B2'),
+ *   )),
+ *   'path/to/file.xls'
+ * );
+ * @endcode
+ *
+ * Or, if ignoring headers:
+ * @code
+ * phpexcel_export(
+ *   NULL,
+ *   array('Worksheet 1' => array(
+ *     array('A1', 'B1'),
+ *     array('A2', 'B2'),
+ *   )),
+ *   'path/to/file.xls',
+ *   array('ignore_headers' => TRUE)
+ * );
+ * @endcode
+ *
+ * phpexcel_export() accepts the following options, which must be given in
+ * array format as the 4th parameter:
+ * - ignore_headers: a boolean indicating whether the headers array should be
+ *   used, or simply ignored. If ignored, worksheet names will be computed
+ *   based on the data parameter.
+ * - template: a path to an existing file, to be used as a template.
+ * - format: The EXCEL format. Can be either 'xls', 'xlsx', 'csv', or 'ods'. By
+ *   default, the extension of the file given as the target path will be used
+ *   (e.g., 'path/to/file.csv' means a format of 'csv'). If the file has no
+ *   extension, or an extension that is not supported, it will fallback to
+ *   'xls'.
+ * - creator: (metadata) The name of the creator of the file.
+ * - title: (metadata) The title of the file.
+ * - subject: (metadata) The subject of the file.
+ * - description: (metadata) The description of the file.
+ *
+ * Any other options will simply be ignored, but can be useful for modules that
+ * implement hook_phpexcel_export().
+ *
+ * phpexcel_export() will always return an integer. This integer can be one of
+ * the following constants:
+ * - PHPEXCEL_SUCCESS: Export was successful.
+ * - PHPEXCEL_ERROR_NO_HEADERS: Error. Returned when no headers are passed, and
+ *   the "ignore_headers" option is different than true.
+ * - PHPEXCEL_ERROR_NO_DATA: Error. Returned when there is no data to export.
+ * - PHPEXCEL_ERROR_PATH_NOT_WRITABLE: Error. Returned when the path is not
+ *   writable.
+ * - PHPEXCEL_ERROR_LIBRARY_NOT_FOUND: Error. Returned when the library could
+ *   not be loaded. Usually this means the library is not in the correct
+ *   location, or not extracted correctly. Remember that the changelog.txt file
+ *   that comes with the source code must be present in the library directory.
+ * - PHPEXCEL_ERROR_FILE_NOT_WRITTEN: Error. Even though the path is writable,
+ *   something prevented PHPExcel from actually saving the file.
+ * - PHPEXCEL_CACHING_METHOD_UNAVAILABLE: Error. This is a configuration error,
+ *   and happens when the site administrator uses an unavailable caching method,
+ *   like Memcached, when there's no Memcached server running.
+ *
+ * @section import Importing data
+ *
+ * Using the module's functions requires the phpexcel.inc file to be loaded:
+ * @code
+ * module_load_include('inc', 'phpexcel');
+ * @endcode
+ *
+ * Importing data is done via phpexcel_import().
+ * @code
+ * $data = phpexcel_import('path/to/file.xls');
+ * @endcode
+ *
+ * This will return the cell data in array format. The array structure is as
+ * follows:
+ * @code
+ * array(
+ *   0 => array(
+ *     0 => array(
+ *       'Header 1' => 'A1',
+ *       'Header 2' => 'B1',
+ *     ),
+ *     1 => array(
+ *       'Header 1' => 'A2',
+ *       'Header 2' => 'B2',
+ *     ),
+ *   ),
+ * );
+ * @endcode
+ *
+ * The 1st dimension is the worksheet(s). The 2nd is the rows. Each row is keyed
+ * by the table header by default.
+ *
+ * It is possible to export the headers as a row of data, and not key the
+ * following rows by these header names by passing FALSE as the second
+ * parameter:
+ * @code
+ * $data = phpexcel_import('path/to/file.xls', FALSE);
+ * @endcode
+ *
+ * This will return the following format:
+ * @code
+ * array(
+ *   0 => array(
+ *     0 => array(
+ *       'Header 1',
+ *       'Header 2',
+ *     ),
+ *     1 => array(
+ *       'A1',
+ *       'B1',
+ *     ),
+ *     2 => array(
+ *       'A2',
+ *       'B2',
+ *     ),
+ *   ),
+ * );
+ * @endcode
+ *
+ * It is also possible to use the worksheet names as keys for the worksheet
+ * data. This is done by passing TRUE as the third parameter.
+ * @code
+ * $data = phpexcel_import('path/to/file.xls', TRUE, TRUE);
+ * @endcode
+ *
+ * This will return the following format:
+ * @code
+ * array(
+ *   'Worksheet 1' => array(
+ *     0 => array(
+ *       'Header 1' => 'A1',
+ *       'Header 2' => 'B1',
+ *     ),
+ *     1 => array(
+ *       'Header 1' => 'A2',
+ *       'Header 2' => 'B2',
+ *     ),
+ *   ),
+ * );
+ * @endcode
+ *
+ * It is possible to specify method calls to the PHPExcel reader before
+ * processing the file data. This is done via the fourth parameter, which is
+ * an array, keyed by method name, and whose value is the parameters. For
+ * instance, if you only want to load specific worksheets to save memory:
+ * @code
+ * $data = phpexcel_import('path/to/file.xls', TRUE, TRUE, array(
+ *   'setLoadSheetsOnly' => array('My sheet'))
+ * );
+ * @endcode
+ *
+ * The specified methods must exist, and are called on an instance of
+ * PHPExcel_Reader_IReader.
+ *
+ * The returned data is either an array (meaning the import was successful) or
+ * an integer. The integer can be one of the following:
+ * - PHPEXCEL_ERROR_FILE_NOT_READABLE: Error. The file was not found or isn't
+ *   readable.
+ * - PHPEXCEL_ERROR_LIBRARY_NOT_FOUND: Error. Returned when the library could
+ *   not be loaded. Usually this means the library is not in the correct
+ *   location, or not extracted correctly. Remember that the changelog.txt file
+ *   that comes with the source code must be present in the library directory.
+ *
+ * @}
+ */
+
+/**
+ * @defgroup phpexcel_perf PHPExcel performance settings
+ * @{
+ * PHPExcel (the library) allows developers to have fine-grained control over
+ * the way cell data is cached. Large Excel files can quickly take up a lot of
+ * memory (about 1Kb per cell), and PHP processes could die running out of
+ * available memory.
+ *
+ * PHPExcel is usable with APC, Memcached, SQLite or static file storage, to
+ * optimize memory usage. PHPExcel can also gzip cell data, resulting in less
+ * memory usage, but at the cost of speed.
+ *
+ * Because this should not be controlled by any modules, but depends on each
+ * site install, there's no API to set what caching method to use. By default,
+ * PHPEXcel will use in-memory caching for fastest performance. Site
+ * administrators can change this by going to /admin/config/system/phpexcel.
+ * This configuration form will allow them to choose an appropriate caching
+ * method, and provide any related settings. The module will then start using
+ * these settings when exporting and importing data, to optimize memory usage.
+ *
+ * The following methods are available:
+ * - In memory, which is the fastest, but also consumes the most memory.
+ * - In memory, serialized. Slightly slower, but decreases memory usage.
+ * - In memory, gzipped. Slighty slower than serialized, but decreases memory
+ *   usage even further.
+ * - APC, requires APC to be installed. Fast, and doesn't increase memory usage.
+ * - Memcache, requires Memcached to be installed and running. Fast, and doesn't
+ *   increase memory usage.
+ * - SQLite3. Ships with most PHP installs. Slow, but doesn't increase memory
+ *   usage.
+ * - Static files (stored in php://tmp). Slowest, but doesn't increase memory
+ *   usage. In this case, it is still possible to set a maximum limit, up until
+ *   which PHPExcel will still use in-memory caching. Defaults to 1Mb. After
+ *   reaching that limit, data is stored in static files.
  * @}
  */
 
